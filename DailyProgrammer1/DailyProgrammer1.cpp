2bc0beb2970035361a78f498b51f0ae84c66bdc6
BryceX <btau20102@yahoo.com>
2015-06-23 02:50:38 -0700

    i forgot to commit this :v:
    lel

---
+++ DailyProgrammer1/DailyProgrammer1.cpp
@@ -0,0 +1,553 @@
+// DailyProgrammer1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+std::string something;
+char holder;
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	
+	std::cin >> something;
+	
+	//std::cout << something << std::endl;
+	for (int i = 0; i < something.length(); i++)
+	{
+		while (something[i + 1] != NULL)
+		{
+			if (something[i] > something[i + 1])
+			{
+				holder = something[i+1];
+				something[i + 1] = something[i];
+				something[i] = holder;
+				
+				
+			}
+
+			break;
+		}
+	}
+	for (int i = 0; i < something.length(); i++)
+	{
+		while (something[i + 1] != NULL)
+		{
+			if (something[i] > something[i + 1])
+			{
+				holder = something[i + 1];
+				something[i + 1] = something[i];
+				something[i] = holder;
+
+
+			}
+
+			break;
+		}
+	}
+	for (int i = 0; i < something.length(); i++)
+	{
+		while (something[i + 1] != NULL)
+		{
+			if (something[i] > something[i + 1])
+			{
+				holder = something[i + 1];
+				something[i + 1] = something[i];
+				something[i] = holder;
+
+
+			}
+
+			break;
+		}
+	}
+	for (int i = 0; i < something.length(); i++)
+	{
+		while (something[i + 1] != NULL)
+		{
+			if (something[i] > something[i + 1])
+			{
+				holder = something[i + 1];
+				something[i + 1] = something[i];
+				something[i] = holder;
+
+
+			}
+
+			break;
+		}
+	}
+	for (int i = 0; i < something.length(); i++)
+	{
+		while (something[i + 1] != NULL)
+		{
+			if (something[i] > something[i + 1])
+			{
+				holder = something[i + 1];
+				something[i + 1] = something[i];
+				something[i] = holder;
+
+
+			}
+
+			break;
+		}
+	}
+	for (int i = 0; i < something.length(); i++)
+	{
+		while (something[i + 1] != NULL)
+		{
+			if (something[i] > something[i + 1])
+			{
+				holder = something[i + 1];
+				something[i + 1] = something[i];
+				something[i] = holder;
+
+
+			}
+
+			break;
+		}
+	}
+	for (int i = 0; i < something.length(); i++)
+	{
+		while (something[i + 1] != NULL)
+		{
+			if (something[i] > something[i + 1])
+			{
+				holder = something[i + 1];
+				something[i + 1] = something[i];
+				something[i] = holder;
+
+
+			}
+
+			break;
+		}
+	}
+	for (int i = 0; i < something.length(); i++)
+	{
+		while (something[i + 1] != NULL)
+		{
+			if (something[i] > something[i + 1])
+			{
+				holder = something[i + 1];
+				something[i + 1] = something[i];
+				something[i] = holder;
+
+
+			}
+
+			break;
+		}
+	}
+	for (int i = 0; i < something.length(); i++)
+	{
+		while (something[i + 1] != NULL)
+		{
+			if (something[i] > something[i + 1])
+			{
+				holder = something[i + 1];
+				something[i + 1] = something[i];
+				something[i] = holder;
+
+
+			}
+
+			break;
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+		for (int i = 0; i < something.length(); i++)
+		{
+			while (something[i + 1] != NULL)
+			{
+				if (something[i] > something[i + 1])
+				{
+					holder = something[i + 1];
+					something[i + 1] = something[i];
+					something[i] = holder;
+
+
+				}
+
+				break;
+			}
+		}
+	}
+	std::cout << something << std::endl;
+	
+
+
+	system("pause");
+}
+
